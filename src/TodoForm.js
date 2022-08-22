f539a31bae2bb2e6ca9739f11e4efda9b3effa9b
Hishaam <hishaamtchirou73@gmail.com>
2022-08-22 20:33:43 +0100

    first commit

---
+++ src/TodoForm.js
@@ -0,0 +1,45 @@
+import {useState, useEffect, useRef} from 'react'
+
+function TodoForm(props) {
+    const [input, setInput] = useState('');
+
+    const inputRef = useRef(null)
+
+    useEffect(() => {
+        inputRef.current.focus()
+    })
+    
+    const handleChange = e => {
+        setInput(e.target.value);
+    };
+
+    const handleSubmit = e => {
+        e.preventDefault();
+
+    props.onSubmit({
+        id: Math.floor(Math.random() * 10000),
+        text: input
+    });
+
+        setInput('');
+    };
+
+  return (
+    <div> 
+        <form className="todo-form" onSubmit={handleSubmit}>
+            <input 
+                type="text" 
+                placeholder='Renseigne moi' 
+                value={input} 
+                name="text" 
+                className='todo-input' 
+                onChange={handleChange}
+                ref={inputRef}
+            />
+            <button className='todo-button'>Ajouter</button>
+        </form>
+    </div>
+  )
+}
+
+export default TodoForm;
